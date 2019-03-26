4a5c4d626d92b5755c89a123c09ca7984f7e9148
jinjuleekr <jinju2415@gmail.com>
2019-03-26 11:15:46 +0900

    change the app name from Lively to jinjuleekr

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Lively.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jinjuleekr.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
